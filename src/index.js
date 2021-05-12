f733224c5de462fdf07709f6ef7114aa08f08cd1
SevenTimes <krab47@yahoo.com>
2021-05-12 23:05:40 +0400

    add tab switch buttons

--- src/index.js
+++ src/index.js
@@ -3,7 +3,39 @@ import fillHome from './modules/fillHome';
 import fillContacts from './modules/fillContacts';
 import fillMenu from './modules/fillMenu';
 
-loadPage();
-// fillHome();
-// fillContacts();
-fillMenu();
+function clearContent() {
+  const main = document.getElementById('main');
+  main.innerHTML = '';
+}
+
+function addEvents() {
+  const btnMenu = document.querySelectorAll('button');
+  btnMenu.forEach((btn) => {
+    btn.addEventListener('click', () => {
+      switch (btn.id) {
+        case 'home':
+          clearContent();
+          fillHome();
+          break;
+        case 'menu':
+          clearContent();
+          fillMenu();
+          break;
+        case 'contact':
+          clearContent();
+          fillContacts();
+          break;
+        default:
+          break;
+      }
+    });
+  });
+}
+
+function init() {
+  loadPage();
+  addEvents();
+  fillHome();
+}
+
+init();
